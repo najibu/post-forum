df0bef0270888392b25cf2ac0c2f1b017ec6606d
Najibu <najibu.nsubuga@vice.com>
2024-05-17 17:49:58 +0100

    editing comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -8,22 +8,10 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function __construct()
     {
-        //
+        $this->authorizeResource(Comment::class, 'comment');
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
     /**
      * Store a newly created resource in storage.
      */
@@ -43,27 +31,17 @@ class CommentController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Comment $comment)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Comment $comment)
     {
-        //
+        $data = $request->validate([
+            'body' =>'required|string|max:2500',
+        ]);
+
+        $comment->update($data);
+
+        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')]);
     }
 
     /**
@@ -71,8 +49,6 @@ class CommentController extends Controller
      */
     public function destroy(Request $request, Comment $comment)
     {
-        $this->authorize('delete', $comment);
-
         $comment->delete();
 
         return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')]);
---
+++ tests/Feature/Controllers/CommentController/UpdateTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use App\Models\User;
+use App\Models\Comment;
+use function Pest\Laravel\put;
+use function Pest\Laravel\actingAs;
+
+it('requires authentication', function () {
+    put(route('comments.update', Comment::factory()->create()))
+        ->assertRedirect(route('login'));
+});
+
+it('can update a comment', function () {
+    $comment = Comment::factory()->create();
+
+    actingAs($comment->user)
+        ->put(route('comments.update', $comment), [
+            'body' => 'This is an updated comment.',
+        ]);
+
+    $this->assertDatabaseHas('comments', [
+        'id' => $comment->id,
+        'body' => 'This is an updated comment.',
+    ]);
+});
+
+
+it('redirects to the post show page', function () {
+    $comment = Comment::factory()->create();
+
+    actingAs($comment->user)
+        ->put(route('comments.update', $comment), [
+            'body' => 'This is an updated comment.',
+        ])
+        ->assertRedirect(route('posts.show', $comment->post_id));
+});
+
+it('redirects to the correct page of comments', function () {
+    $comment = Comment::factory()->create();
+    $page = 2;
+
+    actingAs($comment->user)
+        ->put(route('comments.update', [ 'comment' => $comment, 'page' => $page]), [
+            'body' => 'This is an updated comment.',
+        ])
+        ->assertRedirect(route('posts.show', ['post' => $comment->post_id, 'page' => $page]));
+});
+
+
+it('cannot update a comment from another user', function () {
+    $comment = Comment::factory()->create();
+
+    actingAs(User::factory()->create())
+        ->put(route('comments.update', ['comment' => $comment]), ['body' => 'This is an updated comment.'])
+        ->assertForbidden();
+});
+
+it('requires a valid body', function ($body) {
+    $comment = Comment::factory()->create();
+
+    actingAs($comment->user)
+        ->put(route('comments.update', ['comment' => $comment]), ['body' => $body])
+        ->assertInvalid('body');
+})->with([
+    null,
+    1,
+    1.5,
+    true,
+    str_repeat('a', 2501),
+]);
